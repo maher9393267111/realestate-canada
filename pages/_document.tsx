595e0eb7a15dac6318ee22072b1f312f620bdf6c
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2024-10-22 18:45:46 +0300

    update _cocument seo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,36 +8,20 @@ export default class MyDocument extends Document {
     return (
       <Html lang="ar">
         <Head>
-          <title>TripRex</title>
-          <meta name="title" content=" TripRex" />
-          <meta name="description" content="TripRex" />
-        <meta
-            name="keywords"
-            content="TripRex"
-   
-          />
+          <title>TripRex - Your Travel Companion</title>
+          <meta name="title" content="TripRex - Your Travel Companion" />
+          <meta name="description" content="Discover amazing travel destinations with TripRex." />
+          <meta name="keywords" content="travel, TripRex, destinations, vacation" />
 
           <link rel="manifest" href="/manifest.json" />
-          {/* <link
-            rel="apple-touch-icon"
-              
-            href="https://firebasestorage.googleapis.com/v0/b/test-2221c.appspot.com/o/logo-2.jfif?alt=media&token=8b51a5e3-9500-41ae-806e-10db8d17e38e"
-             sizes="256x256"
-            // "/favicon.ico"
-          ></link> */}
           <meta name="theme-color" content={theme.palette.primary.main} />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Rubik:wght@500&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-          />
 
+          {/* Preload fonts for better performance */}
+          <link rel="preload" href="https://fonts.googleapis.com/css2?family=Rubik:wght@500&display=swap" as="style" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Rubik:wght@500&display=swap" />
 
-
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
 
         </Head>
         <body dir="rtl">
